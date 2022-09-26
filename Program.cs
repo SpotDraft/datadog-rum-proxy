23ad8b46d6440b72cee1be901f2b0b96b8d86c8b
Madhav Bhagat <madhav@spotdraft.com>
2022-09-26 16:46:53 +0530

    Fix missing content type. Correctly forward user-agent and other headers.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+using System.Text;
 using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,42 +18,88 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions{
 });
 
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/proxy", async (HttpRequest request, ILoggerFactory loggerFactory, IHttpClientFactory httpClientFactory) => {
+app.MapPost("/proxy", async (HttpRequest request, ILoggerFactory loggerFactory, IHttpClientFactory httpClientFactory) =>
+{
     var _logger = loggerFactory.CreateLogger("Proxy");
     var _httpClient = httpClientFactory.CreateClient();
     _logger.LogTrace("Entered handler");
 
-        var forwardUrl = request.Query["ddforward"];
-        if (String.IsNullOrEmpty(forwardUrl)) {
-            _logger.LogWarning("Received empty string in ddforward");
-            return Results.BadRequest();
-        }
+    var forwardUrl = request.Query["ddforward"];
+    if (String.IsNullOrEmpty(forwardUrl))
+    {
+        _logger.LogWarning("Received empty string in ddforward");
+        return Results.BadRequest();
+    }
 
-        var remoteIp = request.HttpContext.Connection.RemoteIpAddress;
+    var remoteIp = request.HttpContext.Connection.RemoteIpAddress;
 
-        if (remoteIp == null) {
-            _logger.LogWarning("Empty remote IP!");
-            return Results.BadRequest();
-        }
+    if (remoteIp == null)
+    {
+        _logger.LogWarning("Empty remote IP!");
+        return Results.BadRequest();
+    }
 
-        Uri forwardUri;
-        if (!Uri.TryCreate(forwardUrl, new UriCreationOptions{}, out forwardUri!)) {
-            _logger.LogWarning("Bad URL!");
-            return Results.BadRequest();
-        }
+    Uri forwardUri;
+    if (!Uri.TryCreate(forwardUrl, new UriCreationOptions { }, out forwardUri!))
+    {
+        _logger.LogWarning("Bad URL!");
+        return Results.BadRequest();
+    }
 
-        var requestBody = request.Body;
+    StreamContent content = getContent(request);
 
-        var content = new StreamContent(requestBody);
-        var requestMessage = new HttpRequestMessage{
-            Method = HttpMethod.Post,
-            RequestUri = forwardUri,
-            Headers = {
-                {"X-Forwarded-For", remoteIp.ToString()},
-            },
-            Content = content
-        };
-        await _httpClient.PostAsync(forwardUrl, content);
-        return Results.Ok();
+    var requestMessage = new HttpRequestMessage
+    {
+        Method = HttpMethod.Post,
+        RequestUri = forwardUri,
+        Content = content
+    };
+    addHeaders(request, remoteIp, requestMessage);
+
+    var response = await _httpClient.SendAsync(requestMessage);
+
+    if (!response.IsSuccessStatusCode)
+    {
+        _logger.LogWarning("Call to DD Failed with content: " + await response.Content.ReadAsStringAsync());
+    }
+    return Results.Ok();
 });
 app.Run();
+
+static StreamContent getContent(HttpRequest request)
+{
+    
+    var requestBody = request.Body;
+    var content = new StreamContent(requestBody);
+    var contentType = request.ContentType ?? "text/plain";
+    string charSet = "";
+    if (contentType.Contains(";"))
+    {
+        var splits = contentType.Split(";");
+        contentType = splits[0];
+        charSet = Encoding.UTF8.WebName;
+    }
+    content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+    if (!String.IsNullOrEmpty(charSet))
+    {
+        content.Headers.ContentType.CharSet = charSet;
+    }
+
+    return content;
+}
+
+static void addHeaders(HttpRequest request, System.Net.IPAddress remoteIp, HttpRequestMessage requestMessage)
+{
+    var headersToCopy = new[] { "User-Agent", "Origin", "Referer", "Cookie", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform" };
+    var headerValues = new Dictionary<string, string>();
+
+    foreach (var header in headersToCopy)
+    {
+        if (!String.IsNullOrEmpty(request.Headers[header]))
+        {
+            requestMessage.Headers.Add(header, request.Headers[header].ToString());
+        }
+    }
+    requestMessage.Headers.Add("Accept", "*/*");
+    requestMessage.Headers.Add("X-Forwarded-For", remoteIp.ToString());
+}
